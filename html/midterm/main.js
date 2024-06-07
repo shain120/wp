a6321c60c06d20be322efdb5d5574ee74c80489e
shain120 <sshain1220@gmail.com>
2024-06-07 23:51:36 +0800

    add snake.html

--- html/midterm/main.js
+++ html/midterm/main.js
@@ -131,11 +131,11 @@ function clear_screen() {
 }
 function add_speed(){
     if(level.value == "4"){
-        if(score > 15 && score%4 == 0){
+        if(score > 10 && score%4 == 0){
             speed++;
         }
-    }else if(score > 10 && level.value == "6"){
-        if(score%2 == 0){
+    }else if(level.value == "6"){
+        if(score > 5 && score%2 == 0){
             speed++;
         }
     }else if(level.value == "7"){
